0eb503bbdb91332af4d0a8d3af3c545d979c6279
Carleton <s10247735@connect.np.edu.sg>
2022-11-25 16:35:32 +0800

    moved menu button to top right background image works again but buttoon not able to increase size

---
+++ Main.js
@@ -0,0 +1,19 @@
+
+// //Toggles between showing and hiding the dropdown menu when user clicks
+// function Dropdown(){
+//     document.getElementById("MenuContent").classList.toggle("show");
+// }
+
+// // Close dropdown menu when user clicks away from the menu
+
+// window.onclick = function(event){
+//     if (!event.target.matches('.dropbtn')){
+//         var dropDown = document.getElementsByClassName("Menu-Content");
+//         for (i=0; i < dropDown.length; i++){
+//             var openDropDown = dropDown[i];
+//             if (openDropDown.classList.contains('show')){
+//                 openDropDown.classList.remove('show');
+//             }
+//         }
+//     }
+// }
