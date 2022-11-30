7d9379946b6db3e5014d073a329bc9a6959b267c
Carleton <s10247735@connect.np.edu.sg>
2022-11-30 20:59:42 +0800

    navbar working for all

--- Main.js
+++ Main.js
@@ -17,3 +17,12 @@
 //         }
 //     }
 // }
+
+function myFunction() {
+    var x = document.getElementById("Navbar");
+    if (x.className === "navbar") {
+    x.className += " responsive";
+    } else {
+    x.className = "navbar";
+    }
+}
