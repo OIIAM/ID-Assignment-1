3c158e5943eda5911edaccd70cfbfe5806b1cf14
Carleton <s10247735@connect.np.edu.sg>
2022-12-03 01:48:33 +0800

    info added in mv.html but js code not working

--- Main.js
+++ Main.js
@@ -1,31 +1,4 @@
 
-// //Toggles between showing and hiding the dropdown menu when user clicks
-// function Dropdown(){
-//     document.getElementById("MenuContent").classList.toggle("show");
-// }
-
-// // Close dropdown menu when user clicks away from the menu
-
-// window.onclick = function(event){
-//     if (!event.target.matches('.dropbtn')){
-//         var dropDown = document.getElementsByClassName("Menu-Content");
-//         for (i=0; i < dropDown.length; i++){
-//             var openDropDown = dropDown[i];
-//             if (openDropDown.classList.contains('show')){
-//                 openDropDown.classList.remove('show');
-//             }
-//         }
-//     }
-// }
-/* Nav bar js */
-function myFunction() {
-    var x = document.getElementById("Navbar");
-    if (x.className === "navbar") {
-    x.className += " responsive";
-    } else {
-    x.className = "navbar";
-    }
-}
 /*Gallery*/
 var zoomImg = document.getElementById('zoom-img-id');
 var zoomImgImg = document.getElementById('ZoomImg');
@@ -38,17 +11,29 @@ function ExitZoomIMG(){
     zoomImg.style.display="none";
 }
 
-
-/*Home js 
-const ToggleButton = document.getElementById('menubutton')
-const NavList = document.getElementById('navBar')
-
-ToggleButton.addEventListener('click', () => {
-    NavList.classList.toggle('active');
-})*/
+//NavBar
 const toggleB = document.getElementById('menubutton')
 const NavList = document.getElementById('navList')
 
 toggleB.addEventListener('click', () => {
     NavList.classList.toggle('active');
 })
+
+// Scroll to Top Function
+
+let BTTButton = document.getElementById('BackToTopBtn');
+/*button appear after user scolls*/
+window.onscroll = function(){scrollFunction()};
+function scrollFunction(){
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        BTTButton.style.display = "block";
+    }
+    else{
+        BTTButton.style.display= "none";
+    }
+}
+/* Scroll back to top of page after user clicks */
+function TOPFunction(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
